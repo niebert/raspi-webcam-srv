108b72a1eac8457b3df2c67331a8e6d0b94738da
Engelbert Niehaus <niehausbert@gmail.com>
2020-06-13 17:59:38 +0200

    Update install_motion_srv.sh

--- install_motion_srv.sh
+++ install_motion_srv.sh
@@ -16,7 +16,9 @@ VIDEO_STREAM_QUALILTY="100"
 # Video Stream Size
 VIDEO_SIZE_WIDTH="368"
 VIDEO_SIZE_HEIGHT="288"
-
+## For LogiTech C270 set e.g. size to
+VIDEO_SIZE_WIDTH="1280"
+VIDEO_SIZE_HEIGHT="720"
 
 echo "Install WebCam Server with Motion"
 echo "Step 1: Update Software Repository List" 
